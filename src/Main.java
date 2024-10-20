2fc35806230dbce7ac9a03e15307c2c5eab00b94
Nicholas <39989194+jeria93@users.noreply.github.com>
2024-10-20 23:31:15 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//          Nu ska vi börja programmera
+//          Nu ska VI börja programmera
 //        Min första commit i main branchen
     }
 }
